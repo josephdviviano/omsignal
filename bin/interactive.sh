05d99486b67c5fe19d1dc1a9fc492e5138c743ef
Joseph Viviano <joseph@viviano.ca>
2019-01-18 16:29:09 -0500

    added helper scripts for setting up environment

--- bin/interactive.sh
+++ bin/interactive.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # feature=k80
-msub -N development -A colosse-users -l advres=MILA2019,nodes=1:gpus=1,walltime=15:00 -I
+msub -N development -A colosse-users -l advres=MILA2019,nodes=1:gpus=1,walltime=30:00 -I
 
