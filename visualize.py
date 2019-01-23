f999fc15a2b9e28d779c9f09b182de617e1d34af
Imzad Badri <badredine.sayah@gmail.com>
2019-01-23 17:44:50 -0500

    adding TSNE visualization

--- visualize.py
+++ visualize.py
@@ -1,5 +1,6 @@
 
 from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 import os
 
 # Must switch backend to Agg to be compatible with the queue/singularity.
@@ -25,5 +26,25 @@ def pca(data):
 
     import IPython; IPython.embed()
 
+    
 
 
+
+
+def tsne(data):
+    
+    mdl = TSNE(n_components=2, random_state=0,
+            perplexity=5, learning_rate=10, n_iter=5000)
+    mdl.fit(data.train_X)
+    train_X_emb = mdl.transform(data.train_X)
+    valid_X_emb = mdl.transform(data.valid_X)
+    
+    plt.scatter(train_X_emb[:, 0], train_X_emb[:, 1], c=data.train_y[:, -1])
+    plt.savefig('tsne_train.jpg')
+    plt.close()
+    
+    plt.scatter(valid_X_emb[:, 0], valid_X_emb[:, 1], c=data.valid_y[:, -1])
+    plt savefig('tnse_valid.jpg')
+    plt.close()
+    
+    import IPython; IPython.embed()
