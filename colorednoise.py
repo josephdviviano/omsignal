ebfe294c1ab81d169e2e242515b62e6a07b71e99
Joseph Viviano <joseph@viviano.ca>
2019-01-23 18:15:53 -0500

    added attributions

--- colorednoise.py
+++ colorednoise.py
@@ -1,4 +1,8 @@
-"""Generate colored noise."""
+"""
+Generate colored noise.
+Cloned from https://github.com/felixpatzelt/colorednoise on Jan 23 2019.
+Author: Felix Patzelt, 2014-2017.
+"""
 
 from numpy import concatenate, real, std, abs
 from numpy.fft import ifft, fftfreq
@@ -6,36 +10,36 @@ from numpy.random import normal
 
 def powerlaw_psd_gaussian(exponent, samples, fmin=0):
     """Gaussian (1/f)**beta noise.
-    
+
     Based on the algorithm in:
-    Timmer, J. and Koenig, M.: 
-    On generating power law noise. 
+    Timmer, J. and Koenig, M.:
+    On generating power law noise.
     Astron. Astrophys. 300, 707-710 (1995)
-    
+
     Normalised to unit variance
-    
+
     Parameters:
     -----------
-    
+
     exponent : float
         The power-spectrum of the generated noise is proportional to
-        
+
         S(f) = (1 / f)**beta
         flicker / pink noise:   exponent beta = 1
         brown noise:            exponent beta = 2
-        
-        Furthermore, the autocorrelation decays proportional to lag**-gamma 
+
+        Furthermore, the autocorrelation decays proportional to lag**-gamma
         with gamma = 1 - beta for 0 < beta < 1.
         There may be finite-size issues for beta close to one.
-    
+
     samples : int
         number of samples to generate
-    
+
     fmin : float, optional
         Low-frequency cutoff.
         Default: 0 corresponds to original paper. It is not actually
         zero, but 1/samples.
-        
+
     Returns
     -------
     out : array
@@ -49,11 +53,11 @@ def powerlaw_psd_gaussian(exponent, samples, fmin=0):
     >>> import colorednoise as cn
     >>> y = cn.powerlaw_psd_gaussian(1, 5)
     """
-    
+
     # frequencies (we asume a sample rate of one)
     f = fftfreq(samples)
-    
-    # scaling factor for all frequencies 
+
+    # scaling factor for all frequencies
     ## though the fft for real signals is symmetric,
     ## the array with the results is not - take neg. half!
     s_scale = abs(concatenate([f[f<0], [f[-1]]]))
@@ -63,7 +67,7 @@ def powerlaw_psd_gaussian(exponent, samples, fmin=0):
         if ix < len(f):
             s_scale[ix:] = s_scale[ix]
     s_scale = s_scale**(-exponent/2.)
-    
+
     # scale random power + phase
     sr = s_scale * normal(size=len(s_scale))
     si = s_scale * normal(size=len(s_scale))
