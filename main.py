f59ee898161f6bf2cc2b294f01da7c23ec0ec2f2
Joseph Viviano <joseph@viviano.ca>
2019-01-25 16:38:18 -0500

    reworked main to utilize experiments file

--- main.py
+++ main.py
@@ -2,19 +2,17 @@
 
 import utils
 import visualize
-
+import experiments
 
 def main():
 
-    preproc = utils.Preprocessor()
+    # run experiment
+    results = experiments.lstm()
+
+    # visualizations
     train_data = utils.Data(augmentation=True)
     valid_data = utils.Data(train=False, augmentation=True)
 
-    print('__getitem__: {}'.format(train_data[0]))
-
-    # convert to numpy for data visualization
-    train_data.to_numpy()
-    valid_data.to_numpy()
     visualize.plot_spectra(train_data)
     visualize.pca(train_data)
     visualize.tsne(train_data)
