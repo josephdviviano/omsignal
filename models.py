b3f1d5603fbce2815fd6226b3e77b1e53b37ec41
Joseph Viviano <joseph@viviano.ca>
2019-01-31 19:34:20 -0500

    model now works properly with 4 independenet output layers, however, the code is ugly in that we cannot submit an arbitrary number of prediction tasks to the model beforehand, we have to submit exactly 4. it would be nice to generalize this at some point.

--- models.py
+++ models.py
@@ -1,54 +1,76 @@
 import torch
-
-
-import torch
 import torch.autograd as autograd
 import torch.nn as nn
 import torch.functional as F
 import torch.optim as optim
-
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
+import utils
+
+CUDA = torch.cuda.is_available()
 
 class LSTMClassifier(nn.Module):
 
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_size):
+    def __init__(self, seq_len, emb_dim, hid_dim, out_dims):
+        """
+        output_sizes is a n-element list of sizes, one for each prediction task.
+        """
 
         super(LSTMClassifier, self).__init__()
 
-        self.embedding_dim = embedding_dim
-        self.hidden_dim = hidden_dim
-        self.vocab_size = vocab_size
+        # Check inputs.
+        if len(out_dims) != 4:
+            raise ValueError('out_dims should have length 4.')
 
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=1)
+        self.hid_dim = hid_dim
+        self.emb_dim = emb_dim
+        self.seq_len = seq_len
+        self.out_dims = out_dims
 
-        self.hidden2out = nn.Linear(hidden_dim, output_size)
-        self.softmax = nn.LogSoftmax()
+        # LSTM accepts the inputs.
+        self.lstm = nn.LSTM(seq_len, hid_dim, num_layers=1, batch_first=True)
 
+        # Dropout applied to the last hidden state of the LSTM.
         self.dropout_layer = nn.Dropout(p=0.2)
 
+        self.out1 = nn.Linear(hid_dim, out_dims[0])
+        self.out2 = nn.Linear(hid_dim, out_dims[1])
+        self.out3 = nn.Linear(hid_dim, out_dims[2])
+        self.out4 = nn.Linear(hid_dim, out_dims[3])
 
-    def init_hidden(self, batch_size):
-        return(autograd.Variable(torch.randn(1, batch_size, self.hidden_dim)),
-                        autograd.Variable(torch.randn(1, batch_size, self.hidden_dim)))
+    def _init_hidden(self, batch_size):
+        ht = autograd.Variable(torch.randn(1, batch_size, self.hid_dim))
+        ct = autograd.Variable(torch.randn(1, batch_size, self.hid_dim))
+
+        if CUDA:
+            ht = ht.cuda()
+            ct = ct.cuda()
+
+        return((ht,ct))
 
 
-    def forward(self, batch, lengths):
+    def forward(self, X):
+        """
+        X is size=(batch_size, n_timepoints)
+        lengths is the number of paired sequences in X (typically 1).
+        """
 
-        self.hidden = self.init_hidden(batch.size(-1))
+        # Initialize hidden states of LSTM.
+        self.hidden = self._init_hidden(X.size(0))
 
-        embeds = self.embedding(batch)
-        packed_input = pack_padded_sequence(embeds, lengths)
-        outputs, (ht, ct) = self.lstm(packed_input, self.hidden)
+        _, (ht, ct) = self.lstm(X, self.hidden)
 
         # ht is the last hidden state of the sequences
         # ht = (1 x batch_size x hidden_dim)
         # ht[-1] = (batch_size x hidden_dim)
-        output = self.dropout_layer(ht[-1])
-        output = self.hidden2out(output)
-        output = self.softmax(output)
+        y_hat = self.dropout_layer(ht[-1])
 
-        return output
+        # All output heads operate on the same LSTM state.
+        y_hat_1 = self.out1(y_hat)
+        y_hat_2 = self.out2(y_hat)
+        y_hat_3 = self.out3(y_hat)
+        y_hat_4 = self.out4(y_hat)
+
+        return([y_hat_1, y_hat_2, y_hat_3, y_hat_4])
 
 
