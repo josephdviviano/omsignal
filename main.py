2134dc2dd7cf1c37253edaa262cd575188b41398
Joseph Viviano <joseph@viviano.ca>
2019-02-05 17:20:12 -0500

    minor changes reflecting updates elsewhere

--- main.py
+++ main.py
@@ -22,14 +22,19 @@ def main():
     # run experiment
     results = experiments.lstm()
 
-    # visualizations
-    train_data = utils.Data(augmentation=True)
-    valid_data = utils.Data(train=False, augmentation=True)
+    # Visualizations using non-shuffled data.
+    train_data = utils.Data(train=True, augmentation=True)
+    valid_data = utils.Data(train=False, augmentation=False)
 
-    visualize.plot_spectra(train_data)
+    visualize.spectra(train_data, log=False, name='spectra_train')
+    visualize.spectra(valid_data, log=False, name='spectra_valid')
+    visualize.timeseries(train_data, name='timeseries_train')
+    visualize.timeseries(valid_data, name='timeseries_valid')
     visualize.pca(train_data)
     visualize.tsne(train_data)
 
 
 if __name__ == "__main__":
     main()
+
+
